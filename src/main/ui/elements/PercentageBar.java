fce6401c308c9073686652d47eaadcab43905f5b
Florian <Florian.Becker-f86@ruhr-uni-bochum.de>
2021-02-07 22:54:48 +0100

    added texture functionality to add texture to PercentageBar

--- src/main/ui/elements/PercentageBar.java
+++ src/main/ui/elements/PercentageBar.java
@@ -1,5 +1,6 @@
 package main.ui.elements;
 
+import textures.Texture;
 import textures.TextureReader;
 
 import java.awt.*;
@@ -11,23 +12,37 @@ public class PercentageBar extends UIElement {
     private static final float xOffset = 0.15F;
     private final Color color;
     private final IGetValues values;
+    private final Texture fillTexture;
 
     public PercentageBar(int x, int y, int width, int height, IGetValues values, String color) {
         super(x, y, width, height);
         setTexture(TextureReader.getTextureByString("PERCENTAGE_BAR"));
         this.color = Color.decode(color);
         this.values = values;
+        this.fillTexture = null;
+    }
+
+    public PercentageBar(int x, int y, int width, int height, IGetValues values, Texture fillTexture) {
+        super(x, y, width, height);
+        setTexture(TextureReader.getTextureByString("PERCENTAGE_BAR"));
+        this.color = null;
+        this.values = values;
+        this.fillTexture = fillTexture;
     }
 
     @Override
     public void paint(Graphics2D g) {
-        final Color oldColor = g.getColor();
-        g.setColor(color);
         int percentageWidth = Math.round((((float) values.getValue()) / values.getMax()) * width);
         percentageWidth -= Math.round(percentageWidth * (xOffset * 0.4F));
         final int fixedXPos = Math.round((x * xOffset) + x);
-        g.fillRect(fixedXPos, y, percentageWidth, height);
-        g.setColor(oldColor);
+        if (color == null)
+            g.drawImage(fillTexture.getContent().getImage(), x, y, width, height, null);
+        else {
+            final Color oldColor = g.getColor();
+            g.setColor(color);
+            g.fillRect(fixedXPos, y, percentageWidth, height);
+            g.setColor(oldColor);
+        }
         super.paint(g);
     }
 
